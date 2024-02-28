18c1e789f7438ed3c5c980f07e148fd7ce4811bd
Curryllo <149586390+Curryllo@users.noreply.github.com>
2024-02-28 19:51:38 +0100

    Pantalla MiPerfil

---
+++ src/pages/ChangeNombre.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Changenombre = () => {
+  return (
+    <div className='changenombre'>
+        <h1>Cambiar el nombre</h1>
+    </div>
+  )
+}
+
+export default Changenombre
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./Perfil.css"
+import { useNavigate } from 'react-router-dom'
 
 export const Perfil = () => {
+  const navigate = useNavigate()
   return (
     <div className='perfil'>
-        <h1>Perfil</h1>
+        <div className='Foto'>
+          <button className='BotonFoto' type='button' onClick={() => {navigate("/changefoto")}}>Cambiar la foto</button>
+        </div>
+        <div className='Nombre'>
+          <button className='BotonNombre' type='button' onClick={() => {navigate("/changenombre")}}>Cambiar el nombre</button>
+        </div>
+        <div className='Correo'>
+          <button className='BotonCorreo' type='button' onClick={() => {navigate("/changecorreo")}}>Cambiar el correo</button>
+        </div>
+        <div className='Contrasegna'>
+          <button className='BotonContrasegna' type='button' onClick={() => {navigate("/changepwd")}}>Cambiar la contraseña</button>
+        </div>
     </div>
   )
 }
