3560f8a74a1eab457ca56158d1a27ccf9f39c870
Latrino <pablo_latre15@hotmail.com>
2024-03-03 18:59:03 +0100

    Ajuste Change.css

--- src/pages/ChangeNombre.jsx
+++ src/pages/ChangeNombre.jsx
@@ -20,24 +20,26 @@ export const Changenombre = () => {
 
   return (
     <div className='change'>
-      <h1>Cambiar el nombre</h1>
-      <div>
-        <p className="actual">Nombre actual: UsuarioPrueba</p>
+      <div className='main-element'>
+        <h1>Cambiar el nombre</h1>
+        <div>
+          <p className="actual">Nombre actual: UsuarioPrueba</p>
+        </div>
+        <form className='form' onSubmit={handleGuardarNuevoNombre}>
+          <label className='nuevo' htmlFor="nuevoNombre">Nuevo nombre: </label>
+          <input
+            type="text"
+            className='input'
+            id="nuevoNombre"
+            placeholder='Introduce el nuevo nombre'
+            value={nuevoNombre}
+            onChange={handleNuevoNombreChange}
+          />
+          <button type="submit" className='submit'>
+            Actualizar nombre
+          </button>
+        </form>
       </div>
-      <form className='form' onSubmit={handleGuardarNuevoNombre}>
-        <label className='nuevo' htmlFor="nuevoNombre">Nuevo nombre: </label>
-        <input
-          type="text"
-          className='input'
-          id="nuevoNombre"
-          placeholder='Introduce el nuevo nombre'
-          value={nuevoNombre}
-          onChange={handleNuevoNombreChange}
-        />
-        <button type="submit" className='submit'>
-          Actualizar nombre
-        </button>
-      </form>
     </div>
   )
 }
