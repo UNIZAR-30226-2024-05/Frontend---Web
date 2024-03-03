41832dbf778c766859bfbe8b37853b74ef965356
Curryllo <149586390+Curryllo@users.noreply.github.com>
2024-03-03 12:45:30 +0100

    Intento cambiar texto Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <h1>Sobre Nosotros</h1>
+        <p>Los miembros del equipo del proyecto son todos estudiantes de la Universidad de Zaragoza,
+          que cursan la asignatura de Proyecto Software de 3º del Grado de Ingeniería Informática,
+          cuyos conocimientos previos son los que han aprendido en las diferentes asignaturas del grado,
+          habiendo trabajado con algunas de las tecnologías con las que se van a desarrollar las aplicaciones como 
+          Android Studio. A continuación se muestran los roles que toma cada uno de ellos en el proyecto.</p>
+    </div>
   )
 }
 
