f753a171c1e5d32ed7f59b3d3077573d908f8bce
Curryllo <149586390+Curryllo@users.noreply.github.com>
2024-03-10 17:25:47 +0100

    Funciona

--- src/pages/Reproductor.jsx
+++ src/pages/Reproductor.jsx
@@ -1,42 +1,65 @@
 import React from 'react'
 import portada from "../images/LOTR1.jpg"
-import { PlayIcon, PauseIcon } from "@heroicons/react/20/solid"
+import { Play, Pause } from "heroicons-react"
 import { SpeakerWaveIcon } from "@heroicons/react/24/outline"
 import { useState, useRef, useContext } from 'react';
-
+import "./Reproductor.css"
+import { Howl } from "howler"
 
 const sound = {
     title: "Capitulo",
-    waveType: "Cancion.mp3",
+    waveType: "/Cancion.mp3",
     imageUrl: portada
 };
 
 const Reproductor = () => {
   const [play, setPlay] = useState(false)
+  const [soundInstance, setSoundInstance] = useState(null)
   const MAX = 20;
   const sonidoRef = useRef<HTMLAudioElement>(null)
 
   function toggleAudio(){
     if (play) {
-        sonidoRef.current?.pause()
+        soundInstance.pause()
         setPlay(false)
     } else{
-        sonidoRef.current?.play()
-        setPlay(true)
+      const newSoundInstance = new Howl({
+        src: [sound.waveType],
+        autoplay: true,
+        onend: () => {
+          setPlay(false);
+        }
+      });
+      setSoundInstance(newSoundInstance);
+      setPlay(true);
     }
   }
 
   function handleVolume(e){
     const { value } = e.target
     const volume = Number(value) / MAX
-    sonidoRef.current.volume = volume
+    soundInstance.volume(volume)
   }
 
   return (
-    <div className='player'>
+    <main>
+      <div className='player'>
         <img className='portada'src={sound.imageUrl} />
-        
-    </div>
+        <button className='boton' type='button'
+        onClick={toggleAudio}>
+            {!play ? (
+                <Play  margin-top='10px' size={24}/>
+            ) : (
+                <Pause margin-top='10px' size={24}/>
+            )}
+        </button>
+        <div className='volumen'>
+          <input className='barraVol' type='range' min={0} max={MAX} onChange={(e) => handleVolume(e)}/>
+          <SpeakerWaveIcon className='altavoz' color='grey' height="20px"/>
+        </div>
+      </div>
+    </main>
+    
   );
 }
 
