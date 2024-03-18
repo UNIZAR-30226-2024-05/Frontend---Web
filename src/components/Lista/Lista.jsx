c3629106aedcc47b6f1804cac6d1b88e4a8fd4ad
Latrino <pablo_latre15@hotmail.com>
2024-03-18 10:50:37 +0100

    Barra de búsqueda

--- src/components/Lista/Lista.jsx
+++ src/components/Lista/Lista.jsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from '../../api/axios';
 import './Lista.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Lista = ({libros}) => {
-  
+    
+    const [busqueda, setBusqueda] = useState('');
+    /*
+    URL_CONSULTA = '/audiolibros/genre/:genero'
+
+    const getPeticion = async () => {
+        await axios.get(URL_CONSULTA)
+        .then(response=>{
+            console.log(response.data);
+        }).catch(error=>{
+            console.log(error);
+        })
+    }
+
+    useEffect( () => {
+        getPeticion();
+    },[])
+    */
+    const handleChangeBusqueda = event => {
+        setBusqueda(event.target.value);
+        console.log('Busqueda: ' + event.target.value);
+    } 
+
     return (
     <div className='contenedor-lista'>
-        <div className='buscador'>
-            
+        <div className='buscador-container'>
+            <input className='buscador'
+                placeholder='Búsqueda por nombre de la obra'
+                value={busqueda}
+                onChange={handleChangeBusqueda}
+            />
+            <button className='buttonSearch'>
+                <FontAwesomeIcon icon={faSearch} />
+            </button>
+
         </div>
 
         <div className='lista'>
