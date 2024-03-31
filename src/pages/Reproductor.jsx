fcb87a36763ad4206a438701c8d9c296305f1f6d
Curryllo <149586390+Curryllo@users.noreply.github.com>
2024-03-31 21:53:12 +0200

    Reproductor2

--- src/pages/Reproductor.jsx
+++ src/pages/Reproductor.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import portada from "../images/LOTR1.jpg"
-import { Play, Pause } from "heroicons-react"
+import portada2 from "../images/LOTR2.jpg"
+import { Play, Pause, ChevronDoubleLeft, ChevronDoubleRight } from "heroicons-react"
 import { SpeakerWaveIcon } from "@heroicons/react/24/outline"
 import { useState, useRef, useContext } from 'react';
 import "./Reproductor.css"
 import { Howl } from "howler"
+import {motion} from "framer-motion"
 
 const sound = {
     title: "Capitulo",
     waveType: "/Cancion.mp3",
     imageUrl: portada
 };
+
+const sound2 = {
+  title: "Capitulo",
+  waveType: "/Cancion2.mp3",
+  imageUrl: portada2
+}
 
 const Reproductor = () => {
   const [play, setPlay] = useState(false)
@@ -49,6 +57,26 @@ const Reproductor = () => {
     }
   }
   
+  function skipCancion(){
+    if (currentTime !== null) {
+      // Si hay una posición almacenada, reanuda la reproducción desde esa posición
+      soundInstance.play();
+      soundInstance.seek(currentTime);
+    } else {
+      // Si no hay una posición almacenada, inicia la reproducción desde el principio
+      const newSoundInstance = new Howl({
+        src: [sound2.waveType],
+        autoplay: true,
+        onend: () => {
+          setPlay(false);
+        }
+      });
+      setSoundInstance(newSoundInstance); // Establece la nueva instancia de sonido
+    }
+    // Restablece la posición de reproducción almacenada a null
+    setCurrentTime(null);
+    setPlay(true);
+  }
   
   
 
@@ -63,18 +91,35 @@ const Reproductor = () => {
     <main>
       <div className='player'>
         <img className='portada'src={sound.imageUrl} />
-        <button className='boton' type='button'
-        onClick={toggleAudio}>
-            {!play ? (
-                <Play  margin-top='10px' size={40}/>
-            ) : (
-                <Pause margin-top='10px' size={40}/>
-            )}
-        </button>
+        {/*Botones para el control de la cancion*/}
+        <div className='botones'>
+          <button className='anteriorCancion' type='button'>
+            <ChevronDoubleLeft margin-top='10px' size={40}/>
+          </button>
+          <button className='boton' type='button'
+          onClick={toggleAudio}>
+              {!play ? (
+                  <Play  margin-top='10px' size={40}/>
+              ) : (
+                  <Pause margin-top='10px' size={40}/>
+              )}
+          </button>
+          <button className='siguienteCancion' type='button' onClick={skipCancion}>
+            <ChevronDoubleRight margin-top='10px' size={40}/>
+          </button>
+        </div>
+        <div className='progressbar-container'>
+          <div className='progressbar'>
+            <motion.div className='bar'
+            animate={{width: "75%"}}
+            transition={{duration: 2}}
+                />
+          </div>
+        </div>
         {/*Barra e icono para cambiar el volumen */}
         <div className='volumen'>
           <input className='barraVol' type='range' min={0} max={MAX} onChange={(e) => handleVolume(e)}/>
-          <SpeakerWaveIcon className='altavoz' color='grey' height="20px"/>
+          <SpeakerWaveIcon className='altavoz' color='black' height="20px"/>
         </div>
       </div>
     </main>
