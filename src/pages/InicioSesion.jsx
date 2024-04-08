1d52ceda7530606866fdc711b215f20520611253
Daniel <158586841+daniel-844388@users.noreply.github.com>
2024-04-08 23:49:28 +0200

    Change socket url

--- src/pages/InicioSesion.jsx
+++ src/pages/InicioSesion.jsx
@@ -31,7 +31,7 @@ const InicioSesion = () => {
 
   const connectToSocket = () => {
     // Establecer la conexión con el servidor de Socket.io
-    const socket = io("http://localhost:8000", {
+    const socket = io("https://narratives-backend.azurewebsites.net", {
       withCredentials: true,
       extraHeaders: {
         "my-custom-header": "abcd"
