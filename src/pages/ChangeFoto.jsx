d4ea3a1d5fe98e2329c4bd3ea164bb69fd4f2b72
Curryllo <149586390+Curryllo@users.noreply.github.com>
2024-04-19 14:22:19 +0200

    postFoto2

--- src/pages/ChangeFoto.jsx
+++ src/pages/ChangeFoto.jsx
@@ -65,34 +65,34 @@ export const Changefoto = () => {
           <h1>Elija la foto de perfil</h1>
         </div>
         <div className='rectangulo'>
-            <button className='boton1' onClick={() => handleClick()}>
+            <button className='boton1' onClick={() => handleClick("perro")}>
               <img className='foto1'src={perro}/>
             </button>
-            <button className='boton2' onClick={() => handleClick()}>
+            <button className='boton2' onClick={() => handleClick("gato")}>
               <img className='foto2'src={gato}/>
             </button>
-            <button className='boton3' onClick={() => handleClick()}>
+            <button className='boton3' onClick={() => handleClick("rana")}>
               <img className='foto3'src={rana}/>
             </button>
-            <button className='boton4' onClick={() => handleClick()}>
+            <button className='boton4' onClick={() => handleClick("leon")}>
               <img className='foto4'src={leon}/>
             </button>
-            <button className='boton5' onClick={() => handleClick()}>
+            <button className='boton5' onClick={() => handleClick("pollo")}>
               <img className='foto5'src={pollo}/>
             </button>
-            <button className='boton6' onClick={() => handleClick()}>
+            <button className='boton6' onClick={() => handleClick("vaca")}>
               <img className='foto6'src={vaca}/>
             </button>
-            <button className='boton7' onClick={() => handleClick()}>
+            <button className='boton7' onClick={() => handleClick("buho")}>
               <img className='foto7'src={buho}/>
             </button>
-            <button className='boton8' onClick={() => handleClick()}>
+            <button className='boton8' onClick={() => handleClick("perezoso")}>
               <img className='foto8'src={perezoso}/>
             </button>
-            <button className='boton9' onClick={() => handleClick()}>
+            <button className='boton9' onClick={() => handleClick("doraemon")}>
               <img className='foto9'src={doraemon}/>
             </button>
-            <button className='boton10' onClick={() => handleClick()}>
+            <button className='boton10' onClick={() => handleClick("pikachu")}>
               <img className='foto10'src={pikachu}/>
             </button>
         </div>
