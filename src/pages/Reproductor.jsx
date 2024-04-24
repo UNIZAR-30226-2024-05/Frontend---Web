9efb56279d3ab28dff4a4d9f06e6a7f5063176a4
Curryllo <842545@unizar.es>
2024-04-24 21:50:37 +0200

    Reproductor actualizado botones

--- src/pages/Reproductor.jsx
+++ src/pages/Reproductor.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import portada from "../images/LOTR1.jpg"
-import portada2 from "../images/LOTR2.jpg"
-import { Play, Pause, ChevronDoubleLeft, ChevronDoubleRight } from "heroicons-react"
+import { Play, Pause, ChevronDoubleLeft, ChevronDoubleRight, CakeOutline } from "heroicons-react"
 import { SpeakerWaveIcon } from "@heroicons/react/24/outline"
 import { useState, useRef, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
@@ -10,6 +8,7 @@ import { Howl } from "howler"
 import {motion} from "framer-motion"
 
 
+/*
 const sound = {
     title: "Capitulo",
     waveType: "/Cancion.mp3",
@@ -20,24 +19,33 @@ const sound2 = {
   title: "Capitulo",
   waveType: "/Cancion2.mp3",
   imageUrl: portada2
-}
+}*/
 
 const Reproductor = () => {
 
   const location = useLocation();  
 
-  const capitulo = location.state?.capitulo;
+  const capitulos = location.state?.capitulos;
+  //console.log(capitulos)
   const portada = location.state?.portada;
 
   const [play, setPlay] = useState(false)
   const [soundInstance, setSoundInstance] = useState(null)
   const [currentTime, setCurrentTime] = useState(null);
   const MAX = 20;
+  const [indice, setIndice] = useState(0)
   const sonidoRef = useRef<HTMLAudioElement>(null)
 
 
-  //Función para cambiar el icono de play y pause además del estado del audio
-  function toggleAudio(){
+  function obtenerCapiulo(capitulos, indice){
+    if(indice >= 0 && indice < capitulos.length){
+      return capitulos[indice];
+    }
+  }
+  //console.log(obtenerCapiulo(capitulos, 0));
+
+  //Función para cambiar el icono de play
+  function toggleAudio(capitulo) {
     if (play) {
       // Pausa el audio y almacena la posición de reproducción actual
       soundInstance.pause();
@@ -50,15 +58,21 @@ const Reproductor = () => {
         soundInstance.play();
         soundInstance.seek(currentTime);
       } else {
-        // Si no hay una posición almacenada, inicia la reproducción desde el principio
-        const newSoundInstance = new Howl({
-          src: [capitulo.audio],
-          autoplay: true,
-          onend: () => {
-            setPlay(false);
-          }
-        });
-        setSoundInstance(newSoundInstance); // Establece la nueva instancia de sonido
+        try {
+          // Intenta crear la instancia de sonido
+          const newSoundInstance = new Howl({
+            src: [capitulo.audio],
+            autoplay: true,
+            onend: () => {
+              setPlay(false);
+            }
+          });
+          setSoundInstance(newSoundInstance); // Establece la nueva instancia de sonido
+        } catch (error) {
+          // Maneja cualquier excepción relacionada con la creación del contexto de audio
+          console.error('Error al crear la instancia de sonido:', error);
+          // Puedes realizar alguna acción adicional aquí si es necesario
+        }
       }
       // Restablece la posición de reproducción almacenada a null
       setCurrentTime(null);
@@ -66,27 +80,56 @@ const Reproductor = () => {
     }
   }
   
-  function skipCancion(){
-    if (currentTime !== null) {
-      // Si hay una posición almacenada, reanuda la reproducción desde esa posición
-      soundInstance.play();
-      soundInstance.seek(currentTime);
-    } else {
-      // Si no hay una posición almacenada, inicia la reproducción desde el principio
-      const newSoundInstance = new Howl({
-        src: [sound2.waveType],
-        autoplay: true,
-        onend: () => {
-          setPlay(false);
-        }
-      });
-      setSoundInstance(newSoundInstance); // Establece la nueva instancia de sonido
-    }
-    // Restablece la posición de reproducción almacenada a null
-    setCurrentTime(null);
-    setPlay(true);
+  
+  
+function skipCancion(capitulo, indice) {
+  // Pausa el audio actual
+  soundInstance.pause();
+  setCurrentTime(soundInstance.seek());
+  setPlay(false);
+  
+  // Crea una nueva instancia de sonido para el siguiente capítulo
+  try {
+    const newSoundInstance = new Howl({
+      src: [obtenerCapiulo(capitulos,indice+1).audio],
+      autoplay: false, // No autoplay
+      onend: () => {
+        setPlay(false);
+      }
+    });
+    setSoundInstance(newSoundInstance);
+  } catch (error) {
+    console.error('Error al crear la instancia de sonido:', error);
   }
   
+  // Actualiza el índice
+  setIndice(indice + 1);
+}
+
+  
+  function prevCancion(capitulos,indice){
+    // Pausa el audio actual
+  soundInstance.pause();
+  setCurrentTime(soundInstance.seek());
+  setPlay(false);
+  
+  // Crea una nueva instancia de sonido para el siguiente capítulo
+  try {
+    const newSoundInstance = new Howl({
+      src: [obtenerCapiulo(capitulos,indice-1).audio],
+      autoplay: false, // No autoplay
+      onend: () => {
+        setPlay(false);
+      }
+    });
+    setSoundInstance(newSoundInstance);
+  } catch (error) {
+    console.error('Error al crear la instancia de sonido:', error);
+  }
+  
+  // Actualiza el índice
+  setIndice(indice - 1);
+  }
   
 
   //Función para subir o bajar el volumen
@@ -100,21 +143,21 @@ const Reproductor = () => {
     <main>
       <div className='player'>
         <img className='portada'src={portada} />
+        <h2 margin-top='5%' className='cap'>{capitulos[indice].nombre}</h2>
         {/*Botones para el control de la cancion*/}
         <div className='botones'>
-          <button className='anteriorCancion' type='button'>
-            <ChevronDoubleLeft margin-top='10px' size={40}/>
+          <button className='anteriorCancion' type='button' onClick={() => prevCancion(capitulos, indice)}>
+            <ChevronDoubleLeft margin-top='2%' size={40}/>
           </button>
-          <button className='boton' type='button'
-          onClick={toggleAudio}>
+          <button className='boton' type='button' onClick={() => toggleAudio(obtenerCapiulo(capitulos, indice))}>
               {!play ? (
-                  <Play  margin-top='10px' size={40}/>
+                  <Play  margin-top='2%' size={40}/>
               ) : (
-                  <Pause margin-top='10px' size={40}/>
+                  <Pause margin-top='2%' size={40}/>
               )}
           </button>
-          <button className='siguienteCancion' type='button' onClick={skipCancion}>
-            <ChevronDoubleRight margin-top='10px' size={40}/>
+          <button className='siguienteCancion' type='button' onClick={() => skipCancion(capitulos, indice)}>
+            <ChevronDoubleRight margin-top='2%' size={40}/>
           </button>
         </div>
         <div className='progressbar-container'>
