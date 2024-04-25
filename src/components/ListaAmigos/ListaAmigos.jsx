01d9e2ffd692612f0ecb486473aa2d5476ebc178
alissssia <820574@unizar.es>
2024-04-25 18:33:53 +0200

    Buscador amigos 1

--- src/components/ListaAmigos/ListaAmigos.jsx
+++ src/components/ListaAmigos/ListaAmigos.jsx
@@ -6,82 +6,99 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 import Cookie from 'js-cookie';
 
-const ListaAmigos = ({amigos}) => {
-        
-        const URL_CONSULTA = '/amistad/amigos';
-    
-        const [listaAmigos, setListaAmigos] = useState([]);
-        const [listaShow, setListaShow] = useState([]);
-        const [busqueda, setBusqueda] = useState('');
-    
-        useEffect( () => {
-            if (amigos) {
-                setListaAmigos(amigos);
-                setListaShow(amigos);
-            }
-        }, [amigos]);
-    
-        const handleChangeBusqueda = event => {
-            setBusqueda(event.target.value);
-        } 
+const ListaAmigos = () => {
+    const URL_CONSULTA = '/amistad/lista';
 
-        const handleBusqueda = () => {
-            var resultado = listaAmigos.filter((elemento) => {
-                if (elemento.username.toString().toLowerCase().includes(busqueda.toLowerCase()))
-                {return elemento;}
-            });
-            setListaShow(resultado);
+    const [amigos, setAmigos] = useState([]);
+    const [listaShow, setListaShow] = useState([]);
+    const [busqueda, setBusqueda] = useState('');
+
+    useEffect(() => {
+        fetchAmigos();
+    }, []);
+
+    const fetchAmigos = async () => {
+        try {
+            const response = await axios.get(URL_CONSULTA, { withCredentials: true });
+            setAmigos(response.data.amigos);
+            setListaShow(response.data.amigos);
+        } catch (error) {
+            console.error('Error del servidor:', error.message);
         }
+    };
 
-        const handleKeyPress = (event) => {
-            if (event.key === 'Enter') {
-                handleBusqueda();
+    const handleChangeBusqueda = event => {
+        setBusqueda(event.target.value);
+    };
+
+    const handleBusqueda = () => {
+        const resultado = amigos.filter(amigo => {
+            return amigo.username.toLowerCase().includes(busqueda.toLowerCase());
+        });
+        setListaShow(resultado);
+    };
+
+    const handleKeyPress = event => {
+        if (event.key === 'Enter') {
+            handleBusqueda();
+        }
+    };
+
+    const eliminarAmigo = async index => {
+        try {
+            const token = Cookie.get('token');
+            const response = await axios.post(
+                '/amistad/remove',
+                { other_id: amigos[index].id },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            console.log(response.data.message);
+            fetchAmigos(); // Actualizar la lista después de eliminar un amigo
+        } catch (error) {
+            if (error.response) {
+                console.error('Error del servidor:', error.response.data.error);
+            } else {
+                console.error('Error del servidor:', error.message);
             }
         }
+    };
 
-        const eliminarAmigo = async (index) => {
-            try {
-                const token = Cookie.get('token');
-                const response = axios.post('/amistad/remove', { other_id: amigos[index].id }, { headers: { 'Authorization': `Bearer ${token}` } });
-                console.log(response.data.message); // Mensaje de éxito
-            }
-            catch (error) {
-                if (error.response) {
-                    console.error(error.response.data.error); // Manejar errores específicos del servidor
-                } else {
-                    console.error('Error del servidor:', error.message); // Manejar otros errores
-                }
-            }
-        };
-    
-        return (
-            <div className='lista-amigos'>
-                <div className='buscador-listaAmigos-container'>
-                    <input className='buscador-listaAmigos-container-buscador'
-                        placeholder='Buscar' 
-                        value={busqueda} 
-                        onChange={handleChangeBusqueda}
-                        onKeyDown={handleKeyPress}/>
-                    <button className='buscador-listaAmigos-container-button-search'>
-                        <FontAwesomeIcon icon={faSearch}/>
-                    </button>
-                </div>
-                
-                <div className='lista'>
-                    {listaShow.map((amigo, index) => (
-                        <div className='amigo' key={index}>
-                            <div className='amigo-info'>
-                                <a href='/perfilamigo' className='link-amigo'><img className='foto-amigo' src={amigo.img} alt='Foto de perfil'/></a>
-                                <h2 className='nombre-amigo' ><a href='/perfilamigo' className='link-amigo'>{amigo.username}</a></h2>
-                                <button className='eliminar-amigo' onClick={() => eliminarAmigo(index)}>
-                                    <FontAwesomeIcon icon={faTimes}/>
-                                </button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+    return (
+        <div className="lista-amigos">
+            <div className="buscador-listaAmigos-container">
+                <input
+                    className="buscador-listaAmigos-container-buscador"
+                    placeholder="Buscar"
+                    value={busqueda}
+                    onChange={handleChangeBusqueda}
+                    onKeyDown={handleKeyPress}
+                />
+                <button className="buscador-listaAmigos-container-button-search" onClick={handleBusqueda}>
+                    <FontAwesomeIcon icon={faSearch} />
+                </button>
             </div>
-        );
-    }
+
+            <div className="lista">
+                {listaShow.map((amigo, index) => (
+                    <div className="amigo" key={index}>
+                        <div className="amigo-info">
+                            <a href="/perfilamigo" className="link-amigo">
+                                <img className="foto-amigo" src={amigo.img} alt="Foto de perfil" />
+                            </a>
+                            <h2 className="nombre-amigo">
+                                <a href="/perfilamigo" className="link-amigo">
+                                    {amigo.username}
+                                </a>
+                            </h2>
+                            <button className="eliminar-amigo" onClick={() => eliminarAmigo(index)}>
+                                <FontAwesomeIcon icon={faTimes} />
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default ListaAmigos;
