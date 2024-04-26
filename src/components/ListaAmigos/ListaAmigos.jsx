355c4221004b3cfc4f1ec60d663e3d41a80ea148
alissssia <820574@unizar.es>
2024-04-26 18:11:37 +0200

    Lista usuarios y amigos 5

--- src/components/ListaAmigos/ListaAmigos.jsx
+++ src/components/ListaAmigos/ListaAmigos.jsx
@@ -20,247 +20,229 @@ import pikachu from '../../images/fotos-perfil/pikachu.jpg';
 
 const ListaAmigos = ({usuarios}) => {
     
-        const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [listaUsuarios, setListaUsuarios] = useState(usuarios);
+    const [busqueda, setBusqueda] = useState('');
 
+    const handleChangeBusqueda = (event) => {
+        setBusqueda(event.target.value);
+        filtrar(event.target.value);
+    } 
 
-        const [listaUsuarios, setListaUsuarios] = useState(usuarios);
-        const [busqueda, setBusqueda] = useState('');
-        const [ordenarUsuarios, setOrdenarUsuarios] = useState(false);
-    
-    
-        const handleChangeBusqueda = (event) => {
-            setBusqueda(event.target.value);
-            filtrar(event.target.value);
-        } 
+    const filtrar = (terminoBusqueda) => {
+        var resultado = listaUsuarios.filter((usuario) => {
+            return usuario.username.toString().toLowerCase().includes(terminoBusqueda.toLowerCase());
+        });
+        setListaUsuarios(resultado);
+    }
 
-        
+    const obtenerFotoPerfil = (numero) => {
+        console.log(numero);
+        switch(numero) {
+            case '0':
+                return perro;
+                break;
+            case '1':
+                return gato;
+                break;
+            case '2':
+                return rana;
+                break;
+            case '3':
+                return leon;
+                break;
+            case '4':
+                return pollo;
+                break;
+            case '5':
+                return vaca;
+                break;
+            case '6':
+                return buho;
+                break;
+            case '7':
+                return perezoso;
+                break;
+            case '8':
+                return doraemon;
+                break;
+            case '9':
+                return pikachu;
+                break;
+        }
+    }
 
-        const filtrar = (terminoBusqueda) => {
-            var resultado = listaUsuarios.filter((usuario) => {
-                return usuario.username.toString().toLowerCase().includes(terminoBusqueda.toLowerCase());
+    const obtenerEstado = (numero) => {
+        switch (numero) {
+            case 0:
+                return 'Sois amigos'
+            case 1:
+                return 'Enviar solicitud'
+            case 2:
+                return 'Solicitud enviada'
+            case 3:
+                return 'Aceptar solicitud'
+            default:
+                return '';
+        }
+    }
+
+    const enviarSolicitud = async index => {
+        try {
+            const response = await axios.post(
+                '/amistad/send',
+                { other_id: usuarios[index].id },
+                { withCredentials: true } 
+            );
+            console.log(response.data.message); // Mensaje de éxito
+            // Actualizar la lista después de enviar una solicitud
+            const updatedAmigos = usuarios.map((amigo, i) => {
+                if (i === index) {
+                    return { ...amigo, estado: 2 };
+                }
+                return amigo;
             });
-            setUsuariosFiltrados(resultado);
-            setOrdenarUsuarios(true);
-        }
-
-        const obtenerFotoPerfil = (numero) => {
-            console.log(numero);
-            switch(numero) {
-                case '0':
-                    return perro;
-                    break;
-                case '1':
-                    return gato;
-                    break;
-                case '2':
-                    return rana;
-                    break;
-                case '3':
-                    return leon;
-                    break;
-                case '4':
-                    return pollo;
-                    break;
-                case '5':
-                    return vaca;
-                    break;
-                case '6':
-                    return buho;
-                    break;
-                case '7':
-                    return perezoso;
-                    break;
-                case '8':
-                    return doraemon;
-                    break;
-                case '9':
-                    return pikachu;
-                    break;
+            setListaUsuarios(updatedAmigos);
+        } catch (error) {
+            if (error.response) {
+                console.error(error.response.data.error); // Manejar errores específicos del servidor
+            } else {
+                console.error('Error del servidor:', error.message); // Manejar otros errores
             }
         }
+    };
 
-        
-
-        const obtenerEstado = (numero) => {
-            switch (numero) {
-                case 0:
-                    return 'Sois amigos'
-                case 1:
-                    return 'Enviar solicitud'
-                case 2:
-                    return 'Solictud enviada'
-                case 3:
-                    return 'Aceptar solicitud'
+    const eliminarAmigo = async index => {
+        try {
+            const response = await axios.post(
+                '/amistad/remove',
+                { other_id: usuarios[index].id },
+                { withCredentials: true }
+            );
+            console.log(response.data.message); // Mensaje de éxito
+            // Actualizar la lista después de eliminar un amigo
+            const updatedAmigos = usuarios.filter((amigo, i) => i !== index);
+            setListaUsuarios(updatedAmigos);
+        } catch (error) {
+            if (error.response) {
+                console.error(error.response.data.error); // Manejar errores específicos del servidor
+            } else {
+                console.error('Error del servidor:', error.message); // Manejar otros errores
             }
         }
+    };
 
-        const enviarSolicitud = async index => {
-            try {
-                const response = await axios.post(
-                    '/amistad/send',
-                    { other_id: usuarios[index].id },
-                    { withCredentials: true } 
-                );
-                console.log(response.data.message); // Mensaje de éxito
-                // Actualizar la lista después de enviar una solicitud
-                const updatedAmigos = usuarios.map((amigo, i) => {
-                    if (i === index) {
-                        return { ...amigo, estado: 2 };
-                    }
-                    return amigo;
-                });
-                setListaUsuarios(updatedAmigos);
-            } catch (error) {
-                if (error.response) {
-                    console.error(error.response.data.error); // Manejar errores específicos del servidor
-                } else {
-                    console.error('Error del servidor:', error.message); // Manejar otros errores
+    const cancelarSolicitud = async index => {
+        try {
+            const response = await axios.post(
+                '/amistad/cancel',
+                { other_id: usuarios[index].id },
+                { withCredentials: true }
+            );
+            console.log(response.data.message); // Mensaje de éxito
+            // Actualizar la lista después de cancelar una solicitud
+            const updatedAmigos = usuarios.map((amigo, i) => {
+                if (i === index) {
+                    return { ...amigo, estado: 1 };
                 }
-            }
-        };
-
-        const eliminarAmigo = async index => {
-            try {
-                const response = await axios.post(
-                    '/amistad/remove',
-                    { other_id: usuarios[index].id },
-                    { withCredentials: true }
-                );
-                console.log(response.data.message); // Mensaje de éxito
-                // Actualizar la lista después de eliminar un amigo
-                const updatedAmigos = usuarios.filter((amigo, i) => i !== index);
-                setListaUsuarios(updatedAmigos);
-            } catch (error) {
-                if (error.response) {
-                    console.error(error.response.data.error); // Manejar errores específicos del servidor
-                } else {
-                    console.error('Error del servidor:', error.message); // Manejar otros errores
-                }
-            }
-        };
-
-        const cancelarSolicitud = async index => {
-            try {
-                const response = await axios.post(
-                    '/amistad/cancel',
-                    { other_id: usuarios[index].id },
-                    { withCredentials: true }
-                );
-                console.log(response.data.message); // Mensaje de éxito
-                // Actualizar la lista después de cancelar una solicitud
-                const updatedAmigos = usuarios.map((amigo, i) => {
-                    if (i === index) {
-                        return { ...amigo, estado: 1 };
-                    }
-                    return amigo;
-                });
-                setListaUsuarios(updatedAmigos);
-            } catch (error) {
-                if (error.response) {
-                    console.error(error.response.data.error); // Manejar errores específicos del servidor
-                } else {
-                    console.error('Error del servidor:', error.message); // Manejar otros errores
-                }
+                return amigo;
+            });
+            setListaUsuarios(updatedAmigos);
+        } catch (error) {
+            if (error.response) {
+                console.error(error.response.data.error); // Manejar errores específicos del servidor
+            } else {
+                console.error('Error del servidor:', error.message); // Manejar otros errores
             }
         }
+    }
 
-        const aceptarSolicitud = async index => {
-            try {
-                const response = await axios.post(
-                    '/amistad/accept',
-                    { other_id: usuarios[index].id },
-                    { withCredentials: true }
-                );
-                console.log(response.data.message); // Mensaje de éxito
-                // Actualizar la lista después de aceptar una solicitud
-                const updatedAmigos = usuarios.map((amigo, i) => {
-                    if (i === index) {
-                        return { ...amigo, estado: 0 };
-                    }
-                    return amigo;
-                });
-                setListaUsuarios(updatedAmigos);
-            } catch (error) {
-                if (error.response) {
-                    console.error(error.response.data.error); // Manejar errores específicos del servidor
-                } else {
-                    console.error('Error del servidor:', error.message); // Manejar otros errores
+    const aceptarSolicitud = async index => {
+        try {
+            const response = await axios.post(
+                '/amistad/accept',
+                { other_id: usuarios[index].id },
+                { withCredentials: true }
+            );
+            console.log(response.data.message); // Mensaje de éxito
+            // Actualizar la lista después de aceptar una solicitud
+            const updatedAmigos = usuarios.map((amigo, i) => {
+                if (i === index) {
+                    return { ...amigo, estado: 0 };
                 }
+                return amigo;
+            });
+            setListaUsuarios(updatedAmigos);
+        } catch (error) {
+            if (error.response) {
+                console.error(error.response.data.error); // Manejar errores específicos del servidor
+            } else {
+                console.error('Error del servidor:', error.message); // Manejar otros errores
             }
         }
+    }
 
-        const rechazarSolicitud = async index => {
-            try {
-                const response = await axios.post(
-                    '/amistad/reject',
-                    { other_id: usuarios[index].id },
-                    { withCredentials: true }
-                );
-                console.log(response.data.message); // Mensaje de éxito
-                // Actualizar la lista después de rechazar una solicitud
-                const updatedAmigos = usuarios.filter((amigo, i) => i !== index);
-                setListaUsuarios(updatedAmigos);
-            } catch (error) {
-                if (error.response) {
-                    console.error(error.response.data.error); // Manejar errores específicos del servidor
-                } else {
-                    console.error('Error del servidor:', error.message); // Manejar otros errores
-                }
+    const rechazarSolicitud = async index => {
+        try {
+            const response = await axios.post(
+                '/amistad/reject',
+                { other_id: usuarios[index].id },
+                { withCredentials: true }
+            );
+            console.log(response.data.message); // Mensaje de éxito
+            // Actualizar la lista después de rechazar una solicitud
+            const updatedAmigos = usuarios.filter((amigo, i) => i !== index);
+            setListaUsuarios(updatedAmigos);
+        } catch (error) {
+            if (error.response) {
+                console.error(error.response.data.error); // Manejar errores específicos del servidor
+            } else {
+                console.error('Error del servidor:', error.message); // Manejar otros errores
             }
         }
-
-        useEffect(() => {
-            if (ordenarUsuarios) {
-                const usuariosOrdenador = [...listaUsuarios].sort((a, b) => { a.estado - b.estado });
-                setListaUsuarios(usuariosOrdenador);
-                setOrdenarUsuarios(false);
-            }
-        }, [listaUsuarios , ordenarUsuarios]);
+    }
     
-        return (
-            <div className='lista-amigos'>
-                <div className='buscador-listaAmigos-container'>
-                    <input
-                        className='buscador-listaAmigos-container-buscador'
-                        placeholder='Buscar'
-                        value={busqueda}
-                        onChange={handleChangeBusqueda}
-                    />
-                    <button className='buscador-listaAmigos-container-button-search'>
-                        <FontAwesomeIcon icon={faSearch} />
-                    </button>
-                </div>
-        
-                <div className='lista'>
-                    {listaUsuarios.map((usuario, index) => (
-                        <div className='amigo' key={index}>
-                            <div className='amigo-info'>
-                                <img className='foto-amigo' src={obtenerFotoPerfil(usuario.img)} alt='Foto de perfil' onClick={() => navigate('/perfilamigo')}/>
-                                <h2 className='nombre-amigo'><a href='/perfilamigo' className='link-amigo'>{usuario.username}</a></h2>
-                                {usuario.estado === 0 ? (
-                                    <button onClick={() => eliminarAmigo(index)}>Eliminar amigo</button>
-                                ) : null}
-                                {usuario.estado === 1 ? (
-                                    <button onClick={() => enviarSolicitud(index)}> {obtenerEstado(usuario.estado)} </button>
-                                ) : null}
-                                {usuario.estado === 2 ? (
-                                    <button onClick={() => cancelarSolicitud(index)}> {obtenerEstado(usuario.estado)} </button>
-                                ) : null}
-                                {usuario.estado === 3 ? (
-                                    <div>
-                                        <button onClick={() => aceptarSolicitud(index)}>Aceptar solicitud</button>
-                                        <button onClick={() => rechazarSolicitud(index)}>Rechazar solicitud</button>
-                                    </div>
-                                ) : null}
-                            </div>
-                        </div>
-                    ))}
-                </div>
+    return (
+        <div className='lista-amigos'>
+            <div className='buscador-listaAmigos-container'>
+                <input
+                    className='buscador-listaAmigos-container-buscador'
+                    placeholder='Buscar'
+                    value={busqueda}
+                    onChange={handleChangeBusqueda}
+                />
+                <button className='buscador-listaAmigos-container-button-search'>
+                    <FontAwesomeIcon icon={faSearch} />
+                </button>
             </div>
 
-
-        );
-        
-    }
+            <div className='lista'>
+                {listaUsuarios.map((usuario, index) => (
+                    <div className={'amigo ' + (usuario.estado === 0 ? 'amigo-activo' : '')} key={index}>
+                        <div className='amigo-info'>
+                            <img className='foto-amigo' src={obtenerFotoPerfil(usuario.img)} alt='Foto de perfil' onClick={() => navigate('/perfilamigo')}/>
+                            <h2 className='nombre-amigo'><a href='/perfilamigo' className='link-amigo'>{usuario.username}</a></h2>
+                            {usuario.estado === 0 ? (
+                                <button onClick={() => eliminarAmigo(index)}>Eliminar amigo</button>
+                            ) : null}
+                            {usuario.estado === 1 ? (
+                                <button onClick={() => enviarSolicitud(index)}> {obtenerEstado(usuario.estado)} </button>
+                            ) : null}
+                            {usuario.estado === 2 ? (
+                                <button onClick={() => cancelarSolicitud(index)}> {obtenerEstado(usuario.estado)} </button>
+                            ) : null}
+                            {usuario.estado === 3 ? (
+                                <div>
+                                    <button onClick={() => aceptarSolicitud(index)}>Aceptar solicitud</button>
+                                    <button onClick={() => rechazarSolicitud(index)}>Rechazar solicitud</button>
+                                </div>
+                            ) : null}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );        
+}
 
 export default ListaAmigos;
