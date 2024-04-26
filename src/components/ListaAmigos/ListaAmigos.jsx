a2532b87d6e671f987f09de5d602a9b58ad1117b
Curryllo <842545@unizar.es>
2024-04-26 12:32:00 +0200

    Quitar elemento enlace FotoPerfil Amigos

--- src/components/ListaAmigos/ListaAmigos.jsx
+++ src/components/ListaAmigos/ListaAmigos.jsx
@@ -224,7 +224,7 @@ const ListaAmigos = ({usuarios}) => {
                     {listaUsuarios.map((usuario, index) => (
                         <div className='amigo' key={index}>
                             <div className='amigo-info'>
-                                <a href='/perfilamigo' className='link-amigo'><img className='foto-amigo' src={obtenerFotoPerfil(usuario.img)} alt='Foto de perfil' /></a>
+                                <img className='foto-amigo' src={obtenerFotoPerfil(usuario.img)} alt='Foto de perfil' onClick={() => navigate('/perfilamigo')}/>
                                 <h2 className='nombre-amigo'><a href='/perfilamigo' className='link-amigo'>{usuario.username}</a></h2>
                                 {usuario.estado === 0 ? (
                                     <button onClick={() => eliminarAmigo(index)}>Eliminar amigo</button>
