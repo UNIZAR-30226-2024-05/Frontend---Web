064b02ba2223dab1a47181ff407b1520e5e9beb1
Curryllo <842545@unizar.es>
2024-04-28 11:52:06 +0200

    Revert "ChangeFoto2"
    This reverts commit 75aa6f4859ec210d20cb3487545135b9b140769b.

--- src/pages/ChangeFoto.jsx
+++ src/pages/ChangeFoto.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import "./ChangeFoto.css"
 import perro from "../images/fotos-perfil/perro.jpg"
 import gato from "../images/fotos-perfil/gato.jpg"
