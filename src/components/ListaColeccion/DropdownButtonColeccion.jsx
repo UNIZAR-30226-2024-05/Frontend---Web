cc2b3a5e4dfa7fce8313e9f302ca43069e126098
Latrino <pablo_latre15@hotmail.com>
2024-04-29 22:47:01 +0200

    DDB Coleccion v8

--- src/components/ListaColeccion/DropdownButtonColeccion.jsx
+++ src/components/ListaColeccion/DropdownButtonColeccion.jsx
@@ -6,7 +6,7 @@ const DropdownButtonColeccion = ({libro, coleccion, setColeccion}) => {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  const [option, setOption] = useState('Eliminar de ' + coleccion.coleccion.titulo);
+  const option = 'Eliminar de ' + coleccion?.coleccion.titulo;
 
   const handleToggleDropdown = () => {
     setIsOpen(!isOpen);
