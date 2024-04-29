cc771f7ae5963e8ac8fd801710cd2cfc23db26d1
Latrino <pablo_latre15@hotmail.com>
2024-04-29 21:55:11 +0200

    Quitad DDB Biblioteca, modificar ultima lectura home y DDB Coleccion v2

--- src/components/ListaColeccion/ListaColeccion.jsx
+++ src/components/ListaColeccion/ListaColeccion.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './ListaColeccion.css';
-import DropdownButton from '../DropdownButton/DropdownButton';
+import DropdownButtonColeccion from './DropdownButtonColeccion';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Cookie from 'js-cookie';
