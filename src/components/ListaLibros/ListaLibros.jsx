f6408f61b8f34b5d45db77d03470bd541d79552d
Latrino <pablo_latre15@hotmail.com>
2024-04-30 19:36:53 +0200

    Css Biblioteca autor v5

--- src/components/ListaLibros/ListaLibros.jsx
+++ src/components/ListaLibros/ListaLibros.jsx
@@ -94,7 +94,7 @@ const ListaLibros = ({generos, libros}) => {
                                 <h1>{libro.titulo}</h1>
                             </div>
                             <div className='autor'>
-                                <p>Autor: <span onClick={() => handleAutorClick(libro.autor)} className='info-linkAutor'>{libro.autor}</span></p>
+                                <span onClick={() => handleAutorClick(libro.autor)} className='info-linkAutor'>{libro.autor}</span>
                             </div>
                         </div>
                     </div>
