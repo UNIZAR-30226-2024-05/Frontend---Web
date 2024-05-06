bde5ee9ee6276dbaacd2507bde3b586524e83e96
alissssia <820574@unizar.es>
2024-05-06 18:29:18 +0200

    Lista autores 2

--- src/pages/Autores.jsx
+++ src/pages/Autores.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Autores.css';
 import axios from '../api/axios';
 import ListaAutores from '../components/ListaAutores/ListaAutores';
