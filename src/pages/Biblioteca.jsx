03ca5d136da684b3de333d25295f5602c9229456
Latrino <pablo_latre15@hotmail.com>
2024-05-08 19:35:07 +0200

    Prueba bug Biblioteca v4

--- src/pages/Biblioteca.jsx
+++ src/pages/Biblioteca.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './Biblioteca.css';
 import axios from '../api/axios';
-import ListaColecciones from '../components/ListaColecciones/ListaColecciones';
+import ListaLibros from '../components/ListaLibros/ListaLibros';
 import Footer from '../components/Footer/Footer';
 
 const Biblioteca = () => {
@@ -19,26 +19,25 @@ const Biblioteca = () => {
     'Poesía',
     'Aventuras']);
 
-    const [colecciones, setColecciones] = useState([]);
-  const [loading, setLoading] = useState(true); /* Poner TRUE en que descomente consulta */
+    const URL_CONSULTA = '/audiolibros';
 
-  
+    const [libros, setLibros] = useState([]);
+    const [loading, setLoading] = useState(true);
+
     useEffect( () => {
-        const URL_CONSULTA = '/colecciones';
-
-        async function fetchColecciones(){
-            await axios.get(URL_CONSULTA, {withCredentials: true})
-            .then(response=>{
-                setColecciones(response.data.collections);
-                setLoading(false);
-                console.log(response.data);
-            }).catch(error=>{
-                console.log(error);
-                setLoading(false);
-            })
-        }
-        fetchColecciones();
-    }, []);
+      async function fetchLibros(){
+          await axios.get(URL_CONSULTA, {withCredentials: true})
+          .then(response=>{
+              setLibros(response.data.audiolibros);
+              setLoading(false);
+              console.log(response.data);
+          }).catch(error=>{
+              console.log(error);
+              setLoading(false);
+          })
+      }
+      fetchLibros();
+  }, []);
 
   return (
     <div className='biblioteca'>
@@ -49,7 +48,7 @@ const Biblioteca = () => {
             <p>Loading...</p>
           </div>
           ) : (
-            <ListaColecciones className='lista' colecciones={colecciones} setColecciones={setColecciones} />
+            <ListaLibros className='lista' generos={generos} libros={libros} />
           )}
       </div>
       <div className='biblioteca-footer'>
