65d3e1d80123ce7ed55a04cdd08df7295cdce6fa
alissssia <820574@unizar.es>
2024-05-08 18:46:47 +0200

    Footer 6

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,23 +6,19 @@ import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
-  const BolaNegra = () => {
-    return <FontAwesomeIcon icon={faCircle} style={{ color: 'black', marginRight: '5px' }} />;
-  };
-
   return (
     <div className='footer'>
       <div className='sobreNosotros'>
         <h3>Sobre nosotros</h3>
         <p>Somos el equipo Carol Shaw de la asignatura de Proyecto Software de Ingeniería Informática en la Escuela de Ingeniería y Arquitectura en Zaragoza. A continuación presentamos a sus miembros:</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Pablo Latre Villacampa</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Ismael Martínez Vicens</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Luis Daniel Gómez Sevilla</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Alicia Lázaro Huerta</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Manel Jordá Puig Rubio</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Elizabeth Lilai Naranjo Ventura</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Curro Valero Casajús</p>
-        <p className='sobreNosotros-miembros'><BolaNegra/>Daniel Villarreal Gurrea</p>
+        <p className='sobreNosotros-miembros'>Pablo Latre Villacampa</p>
+        <p className='sobreNosotros-miembros'>Ismael Martínez Vicens</p>
+        <p className='sobreNosotros-miembros'>Luis Daniel Gómez Sevilla</p>
+        <p className='sobreNosotros-miembros'>Alicia Lázaro Huerta</p>
+        <p className='sobreNosotros-miembros'>Manel Jordá Puig Rubio</p>
+        <p className='sobreNosotros-miembros'>Elizabeth Lilai Naranjo Ventura</p>
+        <p className='sobreNosotros-miembros'>Curro Valero Casajús</p>
+        <p className='sobreNosotros-miembros'>Daniel Villarreal Gurrea</p>
       </div>
       <div className='contactos'>
         <h3>Contáctanos</h3>
