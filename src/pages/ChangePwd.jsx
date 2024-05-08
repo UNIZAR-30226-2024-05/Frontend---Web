401b66d7b3af5c8b0c44962aba3ba9d81067a2b0
Curryllo <842545@unizar.es>
2024-05-08 13:53:40 +0200

    Cambiar Passwd 12

--- src/pages/ChangePwd.jsx
+++ src/pages/ChangePwd.jsx
@@ -156,7 +156,7 @@ export const ChangePwd = () => {
             onChange={handleRePwdChange}>
           </input>
           {rePasswordError && <p className="error-message">{rePasswordError}</p>}
-          <button type="submit" className='submit' onClick={() => handleClick(viejoPwd, nuevoPwd)}>
+          <button type="submit" className='submit' onClick={() => handleClick(nuevoPwd, viejoPwd)}>
             Actualizar contraseña
           </button>
         </form>
