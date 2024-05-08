9325ee74011270937164b380e64af22d55bcb871
alissssia <820574@unizar.es>
2024-05-08 18:23:22 +0200

    Footer 1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
+  const BolaNegra = () => {
+    return <FontAwesomeIcon icon={faCircle} style={{ color: 'black', marginRight: '5px' }} />;
+  };
+
   return (
     <div className='footer'>
-      <h1>Sobre Nosotros</h1>
-        <p>Los miembros del equipo del proyecto son todos estudiantes de la Universidad de Zaragoza,
-          que cursan la asignatura de Proyecto Software de 3º del Grado de Ingeniería Informática,
-          cuyos conocimientos previos son los que han aprendido en las diferentes asignaturas del grado,
-          habiendo trabajado con algunas de las tecnologías con las que se van a desarrollar las aplicaciones como 
-          Android Studio. A continuación se muestran los roles que toma cada uno de ellos en el proyecto.</p>
+      <div>
+        <h3>Sobre nosotros</h3>
+        <p>¿Quiénes somos?</p>
+        <p>Somos el equipo Carol Shaw de la asignatura de Proyecto Software de Ingeniería Informática en la Escuela de Ingeniería y Arquitectura en Zaragoza. A continuación presentamos a sus miembros:</p>
+        <p><BolaNegra/>Pablo Latre Villacampa</p>
+        <p><BolaNegra/>Ismael Martínez Vicens</p>
+        <p><BolaNegra/>Luis Daniel Gómez Sevilla</p>
+        <p><BolaNegra/>Alicia Lázaro Huerta</p>
+        <p><BolaNegra/>Manel Jordá Puig Rubio</p>
+        <p><BolaNegra/>Elizabeth Lilai Naranjo Ventura</p>
+        <p><BolaNegra/>Curro Valero Casajús</p>
+        <p><BolaNegra/>Daniel Villarreal Gurrea</p>
+      </div>
+      <div>
+        <h3>Contáctanos</h3>
+        <p><FontAwesomeIcon icon={faEnvelope} style={{ color: 'black', marginRight: '5px' }} />
+          <a href='mailto: narratives.unizar@gmail.com'/>
+        </p>
+        <h3>¿Dónde nos puedes encontrar?</h3>
+        <p><FontAwesomeIcon icon={faMapMarkerAlt}/>María de Luna, 3 - 50018 Zaragoza</p>
+        
+      </div>
     </div>
   )
 }
