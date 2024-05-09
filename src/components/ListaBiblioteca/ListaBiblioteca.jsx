5f49bf567bc17fd05a0b93e90275ee99995c39f3
Latrino <pablo_latre15@hotmail.com>
2024-05-09 12:46:56 +0200

    Prueba bug Biblioteca v15

--- src/components/ListaBiblioteca/ListaBiblioteca.jsx
+++ src/components/ListaBiblioteca/ListaBiblioteca.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './ListaLibros.css';
+import './ListaBiblioteca.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Cookie from 'js-cookie';
