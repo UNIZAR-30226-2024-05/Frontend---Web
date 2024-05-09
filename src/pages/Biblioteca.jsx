11980e176c6e9379670f0f926a38c76dcffd26da
Latrino <pablo_latre15@hotmail.com>
2024-05-09 12:11:15 +0200

    Prueba bug Biblioteca v13

--- src/pages/Biblioteca.jsx
+++ src/pages/Biblioteca.jsx
@@ -1,43 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import './Biblioteca.css';
 import axios from '../api/axios';
-import ListaLibros from '../components/ListaLibros/ListaLibros';
+import ListaColecciones from '../components/ListaColecciones/ListaColecciones';
 import Footer from '../components/Footer/Footer';
 
 const Biblioteca = () => {
-  const [generos, setGeneros] = useState([
-    'Misterio',
-    'Fantasía',
-    'Romance',
-    'Terror',
-    'Ciencia ficción',
-    'Historico',
-    'Infantil',
-    'Mitología',
-    'Humor',
-    'Autoayuda',
-    'Poesía',
-    'Aventuras']);
+  const [colecciones, setColecciones] = useState([]);
+  const [loading, setLoading] = useState(true); /* Poner TRUE en que descomente consulta */
 
-    const URL_CONSULTA = '/audiolibros';
-
-    const [libros, setLibros] = useState([]);
-    const [loading, setLoading] = useState(true);
-
+  
     useEffect( () => {
-      async function fetchLibros(){
-          await axios.get(URL_CONSULTA, {withCredentials: true})
-          .then(response=>{
-              setLibros(response.data.audiolibros);
-              setLoading(false);
-              console.log(response.data);
-          }).catch(error=>{
-              console.log(error);
-              setLoading(false);
-          })
-      }
-      fetchLibros();
-  }, []);
+        const URL_CONSULTA = '/colecciones';
+
+        async function fetchColecciones(){
+            await axios.get(URL_CONSULTA, {withCredentials: true})
+            .then(response=>{
+                setColecciones(response.data.collections);
+                setLoading(false);
+                console.log(response.data);
+            }).catch(error=>{
+                console.log(error);
+                setLoading(false);
+            })
+        }
+        fetchColecciones();
+    }, []);
 
   return (
     <div className='biblioteca'>
@@ -48,7 +35,7 @@ const Biblioteca = () => {
             <p>Loading...</p>
           </div>
           ) : (
-            <ListaLibros className='lista' generos={generos} libros={libros} />
+            <ListaColecciones className='lista' colecciones={colecciones} setColecciones={setColecciones} />
           )}
       </div>
       <div className='biblioteca-footer'>
