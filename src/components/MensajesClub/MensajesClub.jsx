27280eb178a3abf1addfd0f854b1350a365a233c
Latrino <pablo_latre15@hotmail.com>
2024-05-12 21:05:51 +0200

    Chat club

---
+++ src/components/MensajesClub/MensajesClub.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './MensajesClub.css';
+import DropdownButtonColeccion from './DropdownButtonColeccion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight, faSearch } from '@fortawesome/free-solid-svg-icons';
+import Cookie from 'js-cookie';
+
+const MensajesClub = ({ club, setClub }) => {
+
+    const [listaMensajes, setListaMensajes] = useState(club.messages);
+    const [nuevoMensaje, setNuevoMensaje] = useState('');
+
+    /* Actualiza info al actualizar la lista de mensajes */
+    useEffect(() => {
+        setColeccion({...club, messages: listaMensajes})
+        filtrar(busqueda);
+      }, [listaMensajes]);
+
+    
+      const handleChangeNewMsg = event => {
+        setNuevoMensaje(event.target.value);
+    } 
+
+    async function enviarMensaje() {
+        console.log({nuevoMensaje});
+        setNuevoMensaje('');
+    }
+
+    const handleEnviarMensaje = () => {
+        enviarMensaje();
+    }
+
+
+    {/* En que se pueda, cambiar todo lo de libros por una consulta al servidor. */}
+
+    return (
+    <div className='contenedor-mensajes'>
+        <div className='cabecera-container'>
+            
+            <input className='escribir-msg'
+                placeholder='Escriba el mensaje'
+                value={nuevoMensaje}
+                onChange={handleChangeNewMsg}
+            />
+            <button className='button-send' onClick={handleEnviarMensaje}>
+                <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+
+        </div>
+
+        <div className='lista'>
+            {listaMensajes.map((msg, i) => (
+                <div key={i}
+                className='mensaje'>
+                    <div className='contenido-msg'>
+                        <div className='texto-msg'>
+                            <span>{msg.mensaje}</span>
+                        </div>
+                    </div>
+                </div>
+              ))}
+        </div>
+        
+    </div>
+  )
+}
+
+export default MensajesClub
---
+++ src/pages/Club.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import './Club.css';
+import { useLocation } from 'react-router-dom';
+import axios from '../api/axios';
+import Footer from '../components/Footer/Footer';
+import MensajeClub from '../components/MensajesClub/MensajesClub';
+
+const Club = () => {
+
+    const location = useLocation();
+    const id_club = new URLSearchParams(location.search).get('id');
+
+    const [club, setClub] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect( () => {
+        const URL_CLUB = `/club/datos/${id_club}`
+
+      async function fetchLibros(){
+          await axios.get(URL_CLUB, {withCredentials: true})
+          .then(response=>{
+              setClub(response.data);
+              setLoading(false);
+              console.log(response.data);
+          }).catch(error=>{
+              console.log(error);
+              setLoading(false);
+          })
+      }
+      fetchLibros();
+  }, []);
+
+
+
+  return (
+    <div className='club'>
+      <div className='club-container'>
+        <h1 className='title'>{club?.nombre}</h1>
+      {loading ? (
+          <div className='loading-container'>
+            <p>Loading...</p>
+          </div>
+          ) : (
+            <MensajesClub className='lista' club={club} setClub={setClub} />
+          )}
+      </div>
+      <div className='club-footer'>
+        <Footer className='footer' />
+      </div>
+    </div>
+  );
+}
+
+export default Club
