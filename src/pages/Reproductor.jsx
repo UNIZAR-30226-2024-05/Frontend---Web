e34d3a9ad4278805491c33e8750f47b1f9e53d2f
Curryllo <842545@unizar.es>
2024-05-13 22:50:38 +0200

    Cambiar por numCap23

--- src/pages/Reproductor.jsx
+++ src/pages/Reproductor.jsx
@@ -204,6 +204,7 @@ function skipCancion(capitulos, numCap) {
 
 function prevCancion(capitulos, numCap) {
   // Actualiza el índice
+  console.log("Para ver que se la pasa a prev", numCap);
   if (numCap < limitInf) {
     setNumCap(numCap - 1);
     console.log("Después de actualizar numCap en prevCancion", numCap);
