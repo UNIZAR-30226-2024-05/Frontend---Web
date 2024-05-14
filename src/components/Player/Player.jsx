229a4b352cb0ffdba6e6ffe8ddf80956276be290
Curryllo <842545@unizar.es>
2024-05-14 23:02:17 +0200

    ReproductorNuevo16

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -6,10 +6,32 @@ import "./Player.css"
 import { Howl } from "howler"
 import {motion} from "framer-motion"
 
-const Player = ({audioElem,isplaying, setisplaying, currentSong, portada}) => {
+const Player = ({audioElem, isplaying, setisplaying, currentSong, portada, setCurrentSong, capitulos}) => {
 
     const PlayPause = () => {
         setisplaying(!isplaying);
+    }
+
+    const skipBack = () => {
+        const index = capitulos.findIndex(x => x.nombre == currentSong.nombre); 
+        if(index > 0){
+            setCurrentSong(capitulos[index - 1])
+            audioElem.current.currentTime = 0;
+        }
+        else{
+            console.log("Has llegado al limite inferior")
+        }
+    }
+
+    const skiptoNext = () => {
+        const index = capitulos.findIndex(x => x.nombre == currentSong.nombre); 
+        if(index > capitulos.length-1){
+            setCurrentSong(capitulos[index + 1])
+            audioElem.current.currentTime = 0;
+        }
+        else{
+            console.log("Has llegado al limite inferior")
+        }
     }
 
     return(
@@ -24,12 +46,12 @@ const Player = ({audioElem,isplaying, setisplaying, currentSong, portada}) => {
                 </div>
             </div>
             <div className="Controlls">
-                <button className="prev" type="button">
-                    <ChevronDoubleLeft margin-top='2%' size={40}/>
+                <button type="button" >
+                    <ChevronDoubleLeft className="prev" margin-top='2%' size={40} onClick={skipBack}/>
                 </button>
                 <Play className="play" margin-top='2%' size={40} onClick={PlayPause}/>
-                <button className="skip" type="button">
-                    <ChevronDoubleRight margin-top='2%' size={40}/>
+                <button type="button" >
+                    <ChevronDoubleRight className="skip" margin-top='2%' size={40} onClick={skipBack}/>
                 </button>
             </div>
         </div>
