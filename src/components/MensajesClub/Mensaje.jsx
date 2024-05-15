836daafbbcafb44e84ee473c6452bf4381e6c47d
Latrino <pablo_latre15@hotmail.com>
2024-05-15 11:42:57 +0200

    Moificar Libro v25 - Mensaje

--- src/components/MensajesClub/Mensaje.jsx
+++ src/components/MensajesClub/Mensaje.jsx
@@ -11,14 +11,14 @@ const Mensaje = ({ mensaje }) => {
         {mensaje.user_id === user_id ? (
             <div className="msg-container-propio">
                 <div className='texto-msg'>
-                    <span>{msg.mensaje}</span>
+                    <span>{mensaje.mensaje}</span>
                 </div>
             </div>
 
         ) : (
             <div className="msg-container-ajeno">
                 <div className='texto-msg'>
-                    <span>{msg.user_id}</span><span>{msg.mensaje}</span>
+                    <span>{mensaje.user_id}</span><span>{mensaje.mensaje}</span>
                 </div>
             </div>
         )}
