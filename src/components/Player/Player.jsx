43ac4d02799df6f6d2fff118730613ae78133c95
Curryllo <842545@unizar.es>
2024-05-16 18:30:15 +0200

    Cuarto Boton

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -71,6 +71,7 @@ const Player = ({audioElem, isplaying, setisplaying, currentSong, portada, setCu
                 <button type="button" >
                     <ChevronDoubleRight className="skip" margin-top='2%' size={40} onClick={skiptoNext}/>
                 </button>
+                <button type ="button" className="Marcapag" margin-top='2%' size={40}/>
             </div>
         </div>
         
