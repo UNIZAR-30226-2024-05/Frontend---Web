d10f88debc031d92bc5e3c60ebf2ef6288466246
Curryllo <842545@unizar.es>
2024-05-16 20:17:58 +0200

    Crear Marcapaginas 5

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -75,7 +75,10 @@ const Player = ({audioElem, isplaying, setisplaying, currentSong, portada, setCu
     const URL_CREAR = '/marcapaginas/create'
     async function handleClickSubmitMarcapaginas(){
         const capitulo = currentSong.id;
-        const tiempo = audioElem.current.currentTime;
+        const horas = parseInt(audioElem.current.currentTime / 3600);
+        const minutos = parseInt((audioElem.current.currentTime - horas * 3600) / 60);
+        const segundos = parseInt(audioElem.current.currentTime - horas * 3600 - minutos * 60);
+        const tiempo = `${horas}:${minutos}:${segundos}`;
         await axios.post(URL_CREAR,
             JSON.stringify({titulo:nuevoMarcapaginas, capitulo, tiempo}),
         {
