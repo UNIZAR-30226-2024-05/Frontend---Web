25fd90509f39059bcf7d92113c5873a3acc5882a
Latrino <pablo_latre15@hotmail.com>
2024-05-16 15:59:08 +0200

    Modificar reproductor v7

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -65,7 +65,7 @@ const Player = ({audioElem, isplaying, setisplaying, currentSong, portada, setCu
                 <p>{currentSong.nombre}</p>
             </div>
             <div className="Navigation">
-                <div className="NavigationWrapper" onClick={checkWidth} ref={clickRef}>
+                <div className="NavigationWrapper" onClick={() => checkWidth} ref={clickRef}>
                     <div className="seekbar" style={{width: `${currentSong.progress+"%"}`}}></div>
                 </div>
             </div>
